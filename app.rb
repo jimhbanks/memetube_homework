6ec6d2e895e24a528ff77b7bd4564b6028fcb4a5
Jim Banks <iamjimhbanks@me.com>
2015-05-28 08:56:17 +0100

    added show page and delete

--- app.rb
+++ app.rb
@@ -17,6 +17,12 @@ get '/videos/new' do
   erb :new
 end
 
+delete '/videos/:id/delete' do
+  sql = "delete from videos where id = #{params[:id]}"
+  run_sql(sql)
+  redirect to('/videos')
+end
+
 post '/videos' do
   sql = "insert into videos (title, description, url, genre) values ('#{params['title']}', '#{params['description']}', '#{params['url']}', '#{params['genre']}')"
   run_sql(sql)
@@ -30,14 +36,14 @@ get '/videos/:id' do
  erb :show
 end
 
-# def run_sql(sql)
-#   conn = PG.connect(dbname: 'radtube', host: 'localhost')
-#   begin
-#     result = conn.exec(sql)
-#   ensure
-#     conn.close
-#   end
-#   result
-# end
+def run_sql(sql)
+  conn = PG.connect(dbname: 'radtube', host: 'localhost')
+  begin
+    result = conn.exec(sql)
+  ensure
+    conn.close
+  end
+  result
+end
 
 
